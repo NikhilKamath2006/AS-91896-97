3f355a8ea28a41288d4140c699ca6704686fc9b3
NikhilKamath2006 <Nikhil.kamath@my.bdsc.school.nz>
2022-05-27 12:03:31 +1200

    Opens New Window

--- Unit_Convertor.py
+++ Unit_Convertor.py
@@ -1,4 +1,10 @@
+#this program will use tkinter to make a gui program which chnages diffrent types of units
+#importing tkinter
 import tkinter as tk 
 from tkinter import *
+from tkinter import ttk
 
+root = tk.Tk()
+root.title("Unit Convertor")
 
+root.mainloop()
