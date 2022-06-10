17649341f7603169b342130aa98d349b29049708
NikhilKamath2006 <Nikhil.kamath@my.bdsc.school.nz>
2022-06-10 12:00:19 +1200

    Major Chnage, Entry Page

--- Unit_Convertor.py
+++ Unit_Convertor.py
@@ -8,6 +8,43 @@ from tkinter import ttk
 root = Tk()
 root.title("Unit Convertor")
 root.geometry('600x700')
+
+#Function for new window when user wants length conversion
+def length_window():
+    top = Toplevel()
+    top.title("Unit Convertor")
+    top.geometry("400x400")   
+b1 = Button(root, text="Distance", command=length_window)
+b1.place(x=150, y=150)
+
+#Function for new window when user wants liquid conversion
+def liquid_window():
+    top = Toplevel()
+    top.title("Unit Convertor")
+    top.geometry("400x400")   
+b2 = Button(root, text="Distance", command=liquid_window)
+
+#Function for new window when user wants area conversion
+def area_window():
+    top = Toplevel()
+    top.title("Unit Convertor")
+    top.geometry("400x400")   
+b3 = Button(root, text="Distance", command=area_window)
+
+#Function for new window when user wants tmperature conversion
+def temperature_window():
+    top = Toplevel()
+    top.title("Unit Convertor")
+    top.geometry("400x400")   
+b4 = Button(root, text="Distance", command=temperature_window)
+
+#Function for new window when user wants weight conversion
+def weight_window():
+    top = Toplevel()
+    top.title("Unit Convertor")
+    top.geometry("400x400")   
+b5 = Button(root, text="Distance", command=weight_window)
+
 #color of window
 root.configure(bg='white')
 
@@ -22,13 +59,13 @@ distance_conversion_Options = ['Kilometer', 'Meter', 'Centimeter', 'Milimeter',
 root.geometry( "600x200" )
 clicked = StringVar()
 
-# The deafault option shown when button is not clicked
-clicked.set( "Distance" )
 
-#Code for the dropdown 
-drop = OptionMenu( root , clicked , *conversion_options )
-drop.pack()
 
+#Possible conversion Units if chosen conversion is Length
+lengths_units = ['Kilometer', 'Meter', 'Centimeter', 'Milimeter','Mile', 'Inch', 'Foot']
+
+#Dictionary of Length conversion rates
+    
 
 
 
