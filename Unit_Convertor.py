3c46836ebb8540f69c4750ff5e52837b05071296
NikhilKamath2006 <Nikhil.kamath@my.bdsc.school.nz>
2022-06-21 14:57:13 +1200

    Fxing conventions

--- Unit_Convertor.py
+++ Unit_Convertor.py
@@ -8,15 +8,15 @@ import conversion_storage #importing information from other file
 #opening window
 root = tk.Tk()
 root.title('Unit Convertor')
-root.configure(bg='white')
+root.configure(bg='#ccc')
 
 #Making screen size unadjustable
 root.resizable(width=False, height=False)
 
 #lists, i have not moved to a diffrent file since they are better kept here
-typeConversion = ['Length', 'Area', 'Mass']
+conversion_type = ['Length', 'Area', 'Mass']
 
-conversionOptions = [['Kilometer', 'Meter', 'Centimeter', 'Milimeter',
+conversion_options = [['Kilometer', 'Meter', 'Centimeter', 'Milimeter',
                       'Mile', 'Inch', 'Foot'],
                      ['Sq Kilometer', 'Sq Meter', 'Sq Mile',
                       'Sq Foot', 'Sq Inch', 'Acre'],
@@ -26,23 +26,23 @@ conversionOptions = [['Kilometer', 'Meter', 'Centimeter', 'Milimeter',
 
 #clears the options on dropdowns 2 and 3
 def clearOptions(eventObject):
-    firstDrop.set('')
-    secondDrop.set('')
+    first_drop.set('')
+    second_drop.set('')
 
 #label showing title
 typeLabel = Label(root, text='Please select type of conversion', bg='#cccccc')
 typeLabel.grid(row=0, column=1, columnspan=2)
-typeDrop = ttk.Combobox(root, width=40, value=(typeConversion))
-typeDrop.set(typeConversion[0])
+typeDrop = ttk.Combobox(root, width=40, value=(conversion_type))
+typeDrop.set(conversion_type[0])
 typeDrop.grid(row=1, column=1, columnspan=2)
 typeDrop.bind("<<ComboboxSelected>>", clearOptions)
 
 def callback(eventObject):
     abc = eventObject.widget.get()
     typeSelected = typeDrop.get()
-    index = typeConversion.index(typeSelected)
-    firstDrop.config(values=conversionOptions[index])
-    secondDrop.config(values=conversionOptions[index])
+    index = conversion_type.index(typeSelected)
+    first_drop.config(values=conversion_options[index])
+    second_drop.config(values=conversion_options[index])
 
 #code for calculation of units
 def calculateConversion(conversionType, from_unit_type, to_unit_type, value):
@@ -60,13 +60,13 @@ def calculateConversion(conversionType, from_unit_type, to_unit_type, value):
     outLabel.configure(text=new_value)
 
 #design improvment for dropdowns
-firstDrop = ttk.Combobox(root, width=18)
-firstDrop.grid(row=2, column=1, columnspan=1)
-firstDrop.bind('<Button-1>', callback)
+first_drop = ttk.Combobox(root, width=18)
+first_drop.grid(row=2, column=1, columnspan=1)
+first_drop.bind('<Button-1>', callback)
 
-secondDrop = ttk.Combobox(root, width=18)
-secondDrop.grid(row=2, column=2, columnspan=1)
-secondDrop.bind('<Button-1>', callback)
+second_drop = ttk.Combobox(root, width=18)
+second_drop.grid(row=2, column=2, columnspan=1)
+second_drop.bind('<Button-1>', callback)
 
 #code which allows user to enter value
 inputLabel = Label(root, text='Please enter value to covert: ', bg='#cccccc')
@@ -77,7 +77,7 @@ valueEntry.grid(row=4, column=1, columnspan=1)
 
 #submit button which will start the code
 submitButton = Button(root, text="Submit", command=lambda: calculateConversion(
-    typeDrop.get(), firstDrop.get(), secondDrop.get(), valueEntry.get()))
+    typeDrop.get(), first_drop.get(), second_drop.get(), valueEntry.get()))
 submitButton.grid(row=4, column=2)
 
 outLabel = Label(root, text='', bg='#cccccc')
